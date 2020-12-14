e0360a6e9c9a5896ef8369533305a56a939dd386
Anton Maxén <Anton.maxen@gmail.com>
2020-12-14 19:22:22 +0100

    day 6 A and B

---
+++ day6.py
@@ -0,0 +1,69 @@
+def main():
+    with open("files/day6input.txt") as my_file:
+        lines = [line.strip() for line in my_file.readlines()]
+    groups = []
+    group = []
+    for line in lines:
+        if line == '':
+            groups.append(group)
+            group = []
+        else:
+            group.append(line)
+
+    if group:
+        groups.append(group)
+
+    alfabet = "abcdefghijklmnopqrstuvwxyz"
+    groups_score = []
+    for group in groups:
+        answer_dict = {c:False for c in alfabet}
+        for person_answers in group:
+            for answer in person_answers:
+                if answer in answer_dict:
+                    answer_dict[answer] = True
+        score = [k for k, v in answer_dict.items() if v]
+        groups_score.append(score)
+
+    total = 0
+    for score in groups_score:
+        total += len(score)
+
+    return total
+
+
+def part_two():
+    with open("files/day6input.txt") as my_file:
+        lines = [line.strip() for line in my_file.readlines()]
+    groups = []
+    group = []
+    for line in lines:
+        if line == '':
+            groups.append(group)
+            group = []
+        else:
+            group.append(line)
+
+    if group:
+        groups.append(group)
+
+    alfabet = "abcdefghijklmnopqrstuvwxyz"
+    groups_score = []
+    for group in groups:
+        answer_dict = {c:True for c in alfabet}
+        for person_answers in group:
+            for character in answer_dict:
+                if character not in person_answers:
+                    answer_dict[character] = False
+        score = [k for k, v in answer_dict.items() if v]
+        groups_score.append(score)
+
+    total = 0
+    for score in groups_score:
+        total += len(score)
+
+    return total
+
+
+if __name__ == "__main__":
+    print(main())
+    print(part_two())
